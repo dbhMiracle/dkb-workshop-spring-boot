3a7cfa9a1dfd1ce92ce75bb642683559a583fc40
Dennis Bohnstedt Hansen <dbh@miracle.dk>
2022-12-08 14:48:45 +0100

    Added @Value configuration to class

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+
+	@Value("${config:no configuration}")
+    private String config;
+
+    private static int count = 1;
+
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		System.out.println("Ping no: "+count++);
+		return String.format("Greetings from Spring Boot in OpenShift using %s!", config);
 	}
-
 }
